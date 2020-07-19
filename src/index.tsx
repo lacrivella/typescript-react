532c234bc5a11bc111287d1c73d3cd2182ba5229
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-19 15:16:40 -0700

    added dependencies redux react-redux axios and redux-thunk

--- src/index.tsx
+++ src/index.tsx
@@ -5,29 +5,26 @@ interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
-// class App extends React.Component<AppProps> {
-//   state = { counter: 0 };
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
 
-//   onIncrease = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
+  onIncrease = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
-//   onDecrease = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
+  onDecrease = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrease}>Increase</button>
-//         <button onClick={this.onDecrease}>Decrease</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrease}>Increase</button>
+        <button onClick={this.onDecrease}>Decrease</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App color="blue" />, document.querySelector('#root'));
