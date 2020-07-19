725ebe22859b8c2faa804dc08efb24fb7e49f583
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-19 14:41:45 -0700

    example for how to annotate types for functional components

--- src/index.tsx
+++ src/index.tsx
@@ -5,26 +5,29 @@ interface AppProps {
   color?: string;
 }
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
+// class App extends React.Component<AppProps> {
+//   state = { counter: 0 };
 
-  onIncrease = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+//   onIncrease = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
 
-  onDecrease = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
+//   onDecrease = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrease}>Increase</button>
-        <button onClick={this.onDecrease}>Decrease</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrease}>Increase</button>
+//         <button onClick={this.onDecrease}>Decrease</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App color="blue" />, document.querySelector('#root'));
