e25526a9cea24530983713608506e16468161b69
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-19 15:28:53 -0700

    basic App component

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { App } from './components/App';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App color="blue" />, 
+    <App />, 
   </Provider>,
   document.querySelector('#root')
 );
