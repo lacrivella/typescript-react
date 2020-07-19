52848f340535137bf11800b9bcb9ca8b0cf8cf11
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-19 14:35:53 -0700

    reverted back to property definition for state

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,8 @@ interface AppProps {
   color?: string;
 }
 
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { counter: 0 };
-  }
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
 
   onIncrease = (): void => {
     this.setState({ counter: this.state.counter + 1 });
