6eeb085cdbded17518c380b49391e01440c80357
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-20 11:30:24 -0700

    reducer using enums and switch statement

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,5 +1,11 @@
-import { Todo } from '../actions'
+import { Todo, FetchTodosAction } from '../actions';
+import { ActionTypes } from '../actions/types';
 
-export const todosReducer = (state: Todo[] = []) => {
-
-}
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    default:
+      return state;
+  }
+};
