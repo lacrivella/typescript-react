1704ae8d11c77117aaa8e5b5636ea2a45018cddb
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-19 18:29:27 -0700

    created an interface for todos in order to properly annotate what types are given back from the api

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
 
-const url = 'https://jsonplaceholder.tpicode.com/todos';
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<Todo[]>(url);
 
     dispatch({
       type: 'FETCH_TODOS',
