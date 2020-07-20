3ed7001b76d0aa8b323a98227da5257e57d282c3
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-20 15:26:04 -0700

    changed actions to be a type union for less clutter in reducer todos.ts

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,5 +1,9 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 export enum ActionTypes {
   //by default assigns 0 to enums unless given a value
   fetchTodos,
   deleteTodo,
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
