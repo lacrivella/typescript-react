86fa37dc1443eb95f03eb0fc805fd18196a97fa4
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-20 11:00:56 -0700

    made an enum for action types

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
@@ -14,7 +15,7 @@ export const fetchTodos = () => {
     const response = await axios.get<Todo[]>(url);
 
     dispatch({
-      type: 'FETCH_TODOS',
+      type: ActionTypes.fetchTodos,
       payload: response.data,
     });
   };
---
+++ src/actions/types.ts
@@ -0,0 +1,4 @@
+export enum ActionTypes {
+  //by default assigns 0 to enums unless given a value
+  fetchTodos,
+}
