51afded3819f4cbbc0b8c2f22fa18e39f80c9969
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-20 12:05:45 -0700

    created interface for AppProps and passed it as a generic type for the react component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions'
+import { StoreState } from '../reducers'
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+export class App extends React.Component<AppProps> {
   render() {
     return <div>Hello Again</div>;
   }
