e7a2844ac976f3d8d130e2fc4604c835e2f8c9de
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-20 12:36:43 -0700

    added a button that when clicked can render a list of todo titles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,8 +9,23 @@ interface AppProps {
 }
 
 class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <ul><li key={todo.id}>{todo.title}</li></ul>;
+    });
+  }
+
   render() {
-    return <div>Hello Again</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
